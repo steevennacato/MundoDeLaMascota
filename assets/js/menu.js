e5ca39ed9e30cd0cd25bbcfaf6770569eac50049
steevennacato <49173097+steevennacato@users.noreply.github.com>
2021-04-23 17:50:58 -0500

    Add files via upload

---
+++ assets/js/menu.js
@@ -0,0 +1,23 @@
+const grid = new Muuri('.grid', {
+    layout: {
+        rounding: false
+    }
+});
+
+window.addEventListener('load', () => {
+    grid.refreshItems().layout();
+    document.getElementById('grid').classList.add('imagenes-cargadas');
+
+    // Agregamos los listener de los enlaces para filtrar por categoria.
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach((elemento) => {
+        elemento.addEventListener('click', (evento) => {
+            evento.preventDefault();
+            enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+            evento.target.classList.add('activo');
+
+            const categoria = evento.target.innerHTML.toLowerCase();
+            categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+        });
+    });
+});
